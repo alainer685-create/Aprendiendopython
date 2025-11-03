c47546f21a3242ac30cad3744c4a7c7dcc7c1f19
alainer685-create <alainer685@gmail.com>
2025-11-03 18:23:23 -0500

    Create area_rectangulo.py

---
+++ area_rectangulo.py
@@ -0,0 +1,9 @@
+# Pide input al usuario
+largo = int(input("ingresa el largo del rectangulo"))
+ancho = int(input("ingresa el ancho del rectangulo"))
+
+# Calcula el área
+area = largo * ancho
+
+# Muestra el resultado
+print("el área es:", area)
