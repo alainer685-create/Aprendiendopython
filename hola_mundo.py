c50c85589ba30cb063329254d25a2cf3341cb493
alainer685-create <alainer685@gmail.com>
2025-11-03 18:25:44 -0500

    Create hola_mundo.py

---
+++ hola_mundo.py
@@ -0,0 +1,19 @@
+print("hola mundo")
+
+nombre = "Tú nombre" # str
+edad = 39 # int
+altura = 1.75 # float
+es_estudiante = True # bool
+
+print(nombre)
+print(edad + 1) # suma 1 a la edad
+
+a = 10
+b = 3
+suma = a + b
+resta = a - b
+multi = a * b
+div = a / b  # Resultado: 3.333...
+modulo = a % b # Resultado: 1
+
+print(suma, resta, multi, div, modulo)
