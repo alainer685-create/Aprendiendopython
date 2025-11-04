dfc69ed6c07e65da95459d73a19e0e2407b0e36a
alainer685-create <alainer685@gmail.com>
2025-11-04 12:39:47 -0500

    Create condicionales.py

---
+++ condicionales.py
@@ -0,0 +1,10 @@
+edad = 19
+
+if edad >= 18:                     # if condición (código si True)
+    print("Eres mayor de edad.")
+elif 13 <= edad <= 17:             # Opcional, para más casos(código si la otra es True)
+    print("Eres adolescente")  
+else:                              # Opcional, para el resto (código si todo lo anterior es False)
+    print("Eres menor de edad.")
+
+# Operadores de comparación: == (igual), != (diferente), > (mayor), < (menor), >=, <=.
