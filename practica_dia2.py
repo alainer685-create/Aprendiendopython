3acd3532f492a9ddb929bbfdffbd4267327086f4
alainer685-create <alainer685@gmail.com>
2025-11-04 12:41:49 -0500

    Create practica_dia2.py

---
+++ practica_dia2.py
@@ -0,0 +1,19 @@
+# Con if
+numero = int(input("Ingresa un número: "))
+
+if numero % 2 == 0:
+    print("El número es par.")
+else:
+    print("El número es impar")   
+
+# Con for
+print("Contando con for: ")
+for num in range(1, 11):
+    print(num)     
+
+# Con while
+print("Contando con while:")
+contador = 1
+while contador <= 10:
+    print(contador)
+    contador += 1    
